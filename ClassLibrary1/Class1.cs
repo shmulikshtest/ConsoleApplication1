0ee6b4a8cffd1d81e992df00bfe7d9b10bdd12f0
shmuliksh <shmulikshtest@gmail.com>
2017-09-24 09:26:10 +0300

    Initial commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApplication1;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        Class2 c2 = new Class2();
+        Class2 c22 = new Class2();
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        /*static void Main(string[] args)
+        {
+
+            //string sLine = String.Empty;
+
+            //FileStream fs = new FileStream(@"C:\test\123.docx", FileMode.Open, FileAccess.Read);
+
+            ////StreamReader sr = new StreamReader(fs, System.Text.UTF8Encoding.Default);
+            //StreamReader sr = new StreamReader(fs);
+
+            //sLine = sr.ReadToEnd();
+
+            //Document doc = new Document();
+            //doc.LoadFromFile(@"C:\test\1234.docx");
+
+
+            Application application = new Application();
+            Document document = application.Documents.Open(@"C:\test\1234.docx");
+            string allText = document.Content.Text;
+            application.Quit();
+
+            // Loop through all words in the document.
+            // count = document.Words.Count;
+            ////string str = document.;
+            //for (int i = 1; i <= count; i++)
+            //{
+            //    // Write the word.
+            //    string text = document.Words[i].Text;
+            //    Console.WriteLine("Word {0} = {1}", i, text);
+            //}
+            //// Close word.
+
+        }*/
+
+
+         private void readFileContent(string path)
+        {
+            //TextExtractor extractor = new TextExtractor(path);
+            //string text = extractor.ExtractText();
+            //Console.WriteLine(text);
+        }
+        static void Main(string[] args)
+        {
+            Program cs = new Program();
+            string path = @"C:\test\1234.docx";
+            cs.readFileContent(path);
+            Console.ReadLine();
+        }
+    }
+}
+
